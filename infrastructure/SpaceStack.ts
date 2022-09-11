87cc7a321c0906a5343916691b92c611addd0cf7
Ryan Browne <hello@ryanbrowne.io>
2022-09-11 17:20:52 +0100

    Adds basic Lambda function

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,8 +1,16 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs'
+import { join } from 'path'
 
 export class SpaceStack extends Stack{
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props)
+
+    const helloLambda = new Function(this, 'helloLambda', {
+      runtime: Runtime.NODEJS_14_X,
+      code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
+      handler: 'hello.main'
+    })
   }
 }
