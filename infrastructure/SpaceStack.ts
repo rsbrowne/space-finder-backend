e3f395fb13759b34093eb6e600636c93364dad16
Ryan Browne <hello@ryanbrowne.io>
2022-09-12 20:01:51 +0100

    Sets up basic API

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,9 +1,12 @@
 import { Stack, StackProps } from 'aws-cdk-lib'
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway'
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda'
 import { Construct } from 'constructs'
 import { join } from 'path'
 
 export class SpaceStack extends Stack{
+  private api = new RestApi(this, 'SpaceApi')
+
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props)
 
@@ -12,5 +15,9 @@ export class SpaceStack extends Stack{
       code: Code.fromAsset(join(__dirname, '..', 'services', 'hello')),
       handler: 'hello.main'
     })
+
+    const helloLambdaIntegration = new LambdaIntegration(helloLambda)
+    const helloLambdaResource = this.api.root.addResource('hello')
+    helloLambdaResource.addMethod('GET', helloLambdaIntegration)
   }
 }
